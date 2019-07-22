1714758b304b5fd8a34cc0de7a1449940c488b1b
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 18:10:09 -0400

    get rid of all that, back to trying changeOrigin

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,9 @@ module.exports = {
   devServer: {
     allowedHosts: ["10.6.80.13", "services-uswest.skytap.com:29588"],
     disableHostCheck: true,
-    proxy: "http://10.6.80.13",
-    changeOrigin: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization"
+    proxy: {
+      target: "http://10.6.80.13/",
+      changeOrigin: true
     }
   }
 };
