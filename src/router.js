7456a1dcb03b9b2f05d5e03e3e1ce4cf13a2d874
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 15:54:11 -0400

    go to About first

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
-import VueBootstrapTable from "vue2-bootstrap-table2";
 
 Vue.use(Router);
 
@@ -12,15 +11,7 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "table",
-      component: VueBootstrapTable
-    },
-    {
-      path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: About
     },
     {
