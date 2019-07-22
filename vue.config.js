034fc43cb8f69f4df3eb237b0e334200f1d5f1c2
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 18:17:38 -0400

    remove index proxy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 // vue.config.js
 module.exports = {
   devServer: {
-    index: "", // specify to enable root proxying
     allowedHosts: ["10.6.80.13", "services-uswest.skytap.com:29588"],
     disableHostCheck: true,
     proxy: "http://localhost:8081"
