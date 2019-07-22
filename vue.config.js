4b48ddb95f7ee320bd8a065969fc7d6e46491179
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 18:09:10 -0400

    try with headers

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,13 @@ module.exports = {
   devServer: {
     allowedHosts: ["10.6.80.13", "services-uswest.skytap.com:29588"],
     disableHostCheck: true,
-    proxy: {
-      target: "http://10.6.80.13",
-      changeOrigin: true
+    proxy: "http://10.6.80.13",
+    changeOrigin: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization"
     }
   }
 };
