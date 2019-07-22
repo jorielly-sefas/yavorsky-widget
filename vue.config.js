c311b5479464460e6e5669cf8c2a231f783125c1
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 18:07:45 -0400

    try changeOrigin

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ module.exports = {
   devServer: {
     allowedHosts: ["10.6.80.13", "services-uswest.skytap.com:29588"],
     disableHostCheck: true,
-    proxy: "http://10.6.80.13"
+    proxy: {
+      target: "http://10.6.80.13",
+      changeOrigin: true
+    }
   }
 };
