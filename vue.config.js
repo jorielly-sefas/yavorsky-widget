f5f02798b7ffb0f254f39b174999f9ff6601b2ad
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 18:21:52 -0400

    rewrite api with changeorigin?

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,11 @@ module.exports = {
   devServer: {
     allowedHosts: ["10.6.80.13", "services-uswest.skytap.com:29588"],
     disableHostCheck: true,
-    proxy: "http://localhost:8081"
+    proxy: {
+      "http://10.6.80.2:9081/api": {
+        target: "http://10.6.80.2:9081/api",
+        changeOrigin: true
+      }
+    }
   }
 };
