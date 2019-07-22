0bba5e7d95df9cc3b93123b7a4290211024a545a
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 18:24:23 -0400

    edit headers

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,25 @@ module.exports = {
   devServer: {
     allowedHosts: ["10.6.80.13", "services-uswest.skytap.com:29588"],
     disableHostCheck: true,
-    proxy: {
-      "http://10.6.80.2:9081/api": {
-        target: "http://10.6.80.2:9081/api",
-        changeOrigin: true
-      }
+    setup: function(app) {
+      app.all("/*", function(req, res, next) {
+        // CORS headers
+        res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+        res.header(
+          "Access-Control-Allow-Methods",
+          "GET,PUT,POST,DELETE,OPTIONS"
+        );
+        // Set custom headers for CORS
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Content-type,Accept,X-Access-Token,X-Key"
+        );
+        if (req.method == "OPTIONS") {
+          res.status(200).end();
+        } else {
+          next();
+        }
+      });
     }
   }
 };
