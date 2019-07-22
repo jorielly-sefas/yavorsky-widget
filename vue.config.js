f948d2c0ea5be366f7294249d67da353ef7a2c1c
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 18:10:58 -0400

    enable root proxying

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 // vue.config.js
 module.exports = {
   devServer: {
+    index: "", // specify to enable root proxying
     allowedHosts: ["10.6.80.13", "services-uswest.skytap.com:29588"],
     disableHostCheck: true,
     proxy: {
+      context: () => true,
       target: "http://10.6.80.13/",
       changeOrigin: true
     }
