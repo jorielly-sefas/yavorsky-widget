9dbd67ce42da9cae804ce6182112d30817944bbb
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-22 16:17:30 -0400

    remove Axios trimmings from mainjs where we don't need them

--- src/main.js
+++ src/main.js
@@ -4,27 +4,10 @@ import router from "./router";
 import VueBootstrapTable from "@/components/VueBootstrapTable.vue";
 import Home from "@/views/Home";
 import About from "@/views/About";
-import Axios from "axios";
-import qs from "qs";
 
 require("bootstrap/dist/css/bootstrap.min.css");
 require("bootstrap/dist/js/bootstrap.bundle.min.js");
 require("jquery/dist/jquery.min.js");
-
-var renderfu = function(colname, entry) {
-  return (
-    '<div class="btn-group" role="group" >' +
-    '  <button type="button" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="false"></span></button>' +
-    '  <button type="button" class="btn btn-sm btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="false"></span></button>' +
-    "</div><span>" +
-    JSON.stringify(entry) +
-    "</span>"
-  );
-};
-
-var handleRow = function(event, entry) {
-  console.log("CLICK ROW: " + JSON.stringify(entry));
-};
 
 var vm = new Vue({
   el: "#app",
