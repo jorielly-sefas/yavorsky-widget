c4bf1cf5d994da6fa60dc66e4cbb2ad12fbb86d6
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-23 12:46:08 -0400

    add br between summ stats

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,7 @@
                 <button @click="approveJob" class="btn btn-primary"><i class="glyphicon glyphicon-plus-sign"></i>Approve Job</button>
             </div>
             <div class="col-4" style="float:left;">
-                <span>{{ values.length }} Jobs in Queue</span>
+                <span>{{ values.length }} Jobs in Queue</span><br />
                 <span v-if="selected.length > 0">{{ selected.length }} Selected</span>
             </div>
             <vue-bootstrap-table
