cffb6b957317691eea4c3d2970a6c66dee037d92
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-23 17:24:42 -0400

    revert to file/hardcoded in router-link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Jobs</router-link> |
-      <router-link to="{ path: '/file/PPQA_003410_O02_0', params: { id: 'PPQA_003410_O02_0', job: { jobId: '003410' } }">File</router-link>
+      <router-link to="/file/PPQA_003410_O02_0">File</router-link>
     </div>
     <router-view/>
   </div>
