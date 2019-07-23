d4ef25eab9f35175f5445b61b610ed1b261befdd
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-23 12:27:09 -0400

    add selected computed value

--- src/views/About.vue
+++ src/views/About.vue
@@ -45,6 +45,7 @@
 import VueBootstrapTable from "@/components/VueBootstrapTable.vue";
 import Axios from "axios";
 import qs from "qs";
+import { PerformanceObserver } from 'perf_hooks';
 
 const loginData = new FormData();
 loginData.set("user", "hcollin@sefas.com");
@@ -140,6 +141,14 @@ return {
   ],
   values: []
 };
+},
+computed: {
+  selected() {
+    var self = this;
+    return self.values.filter(function(data) {
+      return data.selected === true;
+    });
+  }
 },
 mounted: function() {},
 created: function() {
