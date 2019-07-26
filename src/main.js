c8e9aa5b886833953837acfac2d03199fbfd2e8c
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-26 13:18:19 -0400

    added vuex with vue-ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueBootstrapTable from "@/components/VueBootstrapTable.vue";
 import Home from "@/views/Home";
 import About from "@/views/About";
 import File from "@/views/File";
+import store from './store'
 
 require("bootstrap/dist/css/bootstrap.min.css");
 require("bootstrap/dist/js/bootstrap.bundle.min.js");
@@ -14,6 +15,8 @@ var vm = new Vue({
   el: "#app",
   router,
   render: h => h(App),
+  store,
+
   components: {
     VueBootstrapTable,
     Home,
