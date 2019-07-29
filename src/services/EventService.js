75166f8cd362674a3e186b597072c24649731885
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-29 12:26:15 -0400

    update EventService to match 712 proxy, projector now using ~unimplemented 712 endpoint for docs

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -5,7 +5,7 @@ loginData.set("user", "hcollin@sefas.com");
 loginData.set("appid", "YU1mwM6SUbEapBlytGSc9HH7rfTCMoGlQ98uc3hAhcI3");
 
 const apiClient = axios.create({
-  baseURL: `/api/v1.0/producer_ws/`, //proxied by webpack-dev-server
+  baseURL: `/712/api/v1.0/producer_ws/`, //proxied by webpack-dev-server
   timeout: 10000,
   withCredentials: true
 });
@@ -15,13 +15,11 @@ export default {
     return apiClient.post("/login", loginData);
   },
   getJobs() {
-    return apiClient.get(
-      "/api/v1.0/producer_ws/flask/producer/stages/preprintqa/jobs"
-    );
+    return apiClient.get("/flask/producer/stages/preprintqa/jobs");
   },
   getDocs(fileid) {
     return apiClient.get(
-      "/api/v1.0/producer_ws/flask/projector/documents/" +
+      "/flask/projector/documents/" +
         fileid +
         "?fieldList='" +
         this.$store.columnsForFiles +
