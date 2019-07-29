0e09f663734bf81913ab8b2d2e8a382cb6455606
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-29 12:56:17 -0400

    add approveJobs() and rejectJobs()

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -28,5 +28,37 @@ export default {
         "&key=" +
         fileid
     );
+  },
+  approveJobs(jobsToApprove) {
+    const formData = new FormData();
+    if (jobsToApprove.length > 1) {
+      jobsToApprove.forEach(function(item, index, array) {
+        console.log("job: " + item + " index: " + index + " array: " + array);
+        formData.append("jobid", item.jobId + ",");
+        formData.append("fileNumber", item.fileNumber + ",");
+        formData.append("version", item.version + ",");
+      });
+    } else {
+      formData.set("jobid", jobsToApprove[0].jobId);
+      formData.set("fileNumber", jobsToApprove[0].fileNumber);
+      formData.set("version", jobsToApprove[0].version);
+    }
+    return apiClient.get("/flask/producer/actions/PPQACONFIRM", formData);
+  },
+  rejectJobs(jobsToReject) {
+    const formData = new FormData();
+    if (jobsToReject.length > 1) {
+      jobsToReject.forEach(function(item, index, array) {
+        console.log("job: " + item + " index: " + index + " array: " + array);
+        formData.append("jobid", item.jobId + ",");
+        formData.append("fileNumber", item.fileNumber + ",");
+        formData.append("version", item.version + ",");
+      });
+    } else {
+      formData.set("jobid", jobsToReject[0].jobId);
+      formData.set("fileNumber", jobsToReject[0].fileNumber);
+      formData.set("version", jobsToReject[0].version);
+    }
+    return apiClient.get("/flask/producer/actions/PPQAREJECT", formData);
   }
 };
