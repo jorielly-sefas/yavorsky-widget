d6266a6e0fc1209184c68e1347303373248c7236
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-29 18:05:37 -0400

    import BModal, register globally

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import Home from "@/views/Home";
 import About from "@/views/About";
 import File from "@/views/File";
 import store from "@/store";
-import { BTable } from "bootstrap-vue";
+import { BTable, BModal } from "bootstrap-vue";
 Vue.component("b-table", BTable);
+Vue.component("b-modal", BModal);
 
 require("bootstrap/dist/css/bootstrap.min.css");
 require("bootstrap/dist/js/bootstrap.bundle.min.js");
@@ -23,6 +24,7 @@ var vm = new Vue({
     Home,
     About,
     File,
-    BTable
+    BTable,
+    BModal
   }
 }).$mount("#app");
