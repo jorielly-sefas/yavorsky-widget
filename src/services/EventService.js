dbc8812a8fc7073ac2965c63def6030c934e6150
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-29 12:03:01 -0400

    move loginData to login() function

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -6,13 +6,12 @@ loginData.set("appId", "YU1mwM6SUbEapBlytGSc9HH7rfTCMoGlQ98uc3hAhcI3");
 
 const apiClient = axios.create({
   baseURL: `/api/v1.0/producer_ws/`, //proxied by webpack-dev-server
-  data: loginData,
   timeout: 10000
 });
 
 export default {
   login() {
-    return apiClient.post("/login");
+    return apiClient.post("/login", loginData);
   },
   getJobs() {
     return apiClient.get(
