53949c79c9d0a996df1f550e49f7d95890b77b58
Mike Yavorsky <me@mikeyavorsky.com>
2019-07-30 15:24:03 -0400

    revert change to checkbox div

--- src/views/File.vue
+++ src/views/File.vue
@@ -107,18 +107,17 @@
           </template>
           <template slot="select" slot-scope="data" v-html="data.value">
             <td v-if="showSelect">
-              <input
-                type="checkbox"
-                class="custom-control-input"
-                :id="'check' + data.index"
-              />
-              <label
-                class="custom-control-label"
-                :for="'check' + data.index"
-              ></label>
-              <!--<div class="form-check">
-                                <input class="form-check-input position-static" type="checkbox" aria-label="Select All" v-model="entry.selected">
-								</div>-->
+              <div class="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  class="custom-control-input"
+                  :id="'check' + data.index"
+                />
+                <label
+                  class="custom-control-label"
+                  :for="'check' + data.index"
+                ></label>
+              </div>
             </td>
           </template>
           <template slot="pull" slot-scope="data" v-html="data.value">
