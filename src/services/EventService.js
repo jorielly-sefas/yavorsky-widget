9bc4ca6fec2e1e80c1248e7b858399879b7eaece
Mike Yavorsky <me@mikeyavorsky.com>
2019-08-01 23:36:05 -0400

    ternary op for perPage on getDocs() in EventSrevice

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -28,7 +28,7 @@ export default {
       "?fieldList='" +
       columnsForFiles +
       "'&pageSize=" +
-      100 + //replace with this.$store.currentPageSize
+      (this.$store.storedPerPage ? this.$store.storedPerPage : 20) + //replace with this.$store.currentPageSize
         "&key=" +
         fileid
     );
