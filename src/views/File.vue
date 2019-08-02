1c8cb758e6b4c2a0dd97e9c446c829afdf8a02b8
Mike Yavorsky <me@mikeyavorsky.com>
2019-08-02 09:29:51 -0400

    docs.selected?

--- src/views/File.vue
+++ src/views/File.vue
@@ -153,7 +153,7 @@
                     type="checkbox"
                     class="custom-control-input"
                     :id="'check' + row.mailpiece_id"
-                    v-model="this.$store.state.docs.findIndex(row).selected"
+                    v-model="docs.selected"
                   />
                   <label
                     class="custom-control-label"
