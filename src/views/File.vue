e52ee8d2129c122011348df3a7d86bc0013c8fed
Mike Yavorsky <me@mikeyavorsky.com>
2019-08-04 19:51:57 -0400

    spread click.native around

--- src/views/File.vue
+++ src/views/File.vue
@@ -49,7 +49,7 @@
                 type="button"
                 class="btn btn-outline-primary"
                 v-if="(docs ? selectedDocs.length : 0) > 0"
-                v-on:click="pullDocs"
+                v-on:click.native="$emit('multi-pull-placeholder')"
               >
                 Pull {{ selectedDocs.length }}
               </button>
@@ -61,7 +61,7 @@
                   type="button"
                   class="btn btn-outline-primary"
                   v-if="(docs ? selectedDocs.length : 0) > 0"
-                  v-on:click.native="$emit('pull-docs')"
+                  v-on:click.native="$emit('multi-boolean-placeholder')"
                   :key="action"
                 >
                   {{ action.name }} {{ selectedDocs.length }}
@@ -71,7 +71,7 @@
                 type="button"
                 class="btn btn-outline-primary"
                 v-if="(docs ? selectedDocs.length : 0) > 0"
-                v-on:click="$emit('view-docs')"
+                v-on:click.native="$emit('view-docs-placeholder')"
               >
                 View {{ selectedDocs.length }}
               </button>
@@ -168,7 +168,7 @@
             <button @click="viewDocs(data.item.mailpiece_id)">View PDF</button>
           </template>
           <template slot="pull" slot-scope="data" v-html="data.value">
-            <button @click="$emit('pull-doc', data.item.mailpiece_id)">
+            <button @click.native="$emit('pull-doc', data.item.mailpiece_id)">
               Pull
             </button>
           </template>
@@ -181,7 +181,9 @@
           >
             <button
               :key="action"
-              v-on:click="takeBooleanAction(action, data.item.mailpiece_id)"
+              v-on:click.native="
+                takeBooleanAction(action, data.item.mailpiece_id)
+              "
             >
               {{ action.value }}
             </button>
