2707b9c26fd0bebfb193d20411d95a282d330995
Mike Yavorsky <me@mikeyavorsky.com>
2019-08-04 20:42:14 -0400

    this..getters.getDocWithId?

--- src/views/File.vue
+++ src/views/File.vue
@@ -518,7 +518,7 @@ export default {
     // },
     pullDoc(id) {
       console.log("pull doc method called");
-      let item = getDocWithId(id);
+      let item = this.$store.getters.getDocWithId(id);
       var docData = [
         {
           oldDoc: {
